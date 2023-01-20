3b4b0839d8b6ae601c960b7e5b2f511ed9de09f8
Kade Robertson <kade@kaderobertson.dev>
2023-01-20 15:35:50 -0500

    test: verify caching behaviour

--- src/cache_middleware.rs
+++ src/cache_middleware.rs
@@ -31,3 +31,63 @@ impl Middleware for CacheMiddleware {
         Response::new(200, "OK", &response_str)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use mockito::{self, mock};
+    use std::{env::temp_dir, fs::remove_dir_all};
+    use ureq::AgentBuilder;
+
+    #[test]
+    fn first_request_creates_cache() {
+        let _m = mock("GET", "/file.txt")
+            .with_status(200)
+            .with_body("some example text")
+            .create();
+
+        let full_url = format!("{}/file.txt", mockito::server_url());
+
+        let cache_dir = temp_dir().join("test01");
+        let _ = remove_dir_all(&cache_dir);
+
+        let agent = AgentBuilder::new()
+            .middleware(CacheMiddleware::new(&cache_dir.to_string_lossy()))
+            .build();
+
+        let response = agent.get(&full_url).call().unwrap();
+        assert_eq!(response.status(), 200);
+        assert_eq!(response.into_string().unwrap(), "some example text");
+        assert!(cache_dir.read_dir().unwrap().next().is_some());
+    }
+
+    #[test]
+    fn second_request_reads_cache() {
+        let full_url = format!("{}/file.txt", mockito::server_url());
+
+        let cache_dir = temp_dir().join("test02");
+        let _ = remove_dir_all(&cache_dir);
+
+        let agent = AgentBuilder::new()
+            .middleware(CacheMiddleware::new(&cache_dir.to_string_lossy()))
+            .build();
+
+        let _m = mock("GET", "/file.txt")
+            .with_status(200)
+            .with_body("some example text")
+            .create();
+
+        let _ = agent.get(&full_url).call().unwrap();
+
+        // Removes the previous mock so this URL shouldn't work.
+        mockito::reset();
+        assert!(agent
+            .get(&format!("{}/other-file.txt", mockito::server_url()))
+            .call()
+            .is_err());
+
+        let response = agent.get(&full_url).call().unwrap();
+        assert_eq!(response.status(), 200);
+        assert_eq!(response.into_string().unwrap(), "some example text");
+    }
+}
