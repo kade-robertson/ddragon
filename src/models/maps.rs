6479c47097cbcfa8e6ce638942de45cb23906374
Kade <kade-robertson@users.noreply.github.com>
2023-01-20 16:25:36 -0500

    feat: support mission assets (#12)
    * fix(maps): use existing image struct
    
    * feat: support mission assets

--- src/models/maps.rs
+++ src/models/maps.rs
@@ -1,30 +1,19 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
-#[derive(Serialize, Deserialize)]
+use super::shared::Image;
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Maps {
-    #[serde(rename = "type")]
-    pub maps_type: String,
     pub version: String,
     pub data: HashMap<String, Map>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Map {
     #[serde(rename = "MapName")]
     pub map_name: String,
     #[serde(rename = "MapId")]
     pub map_id: String,
-    pub image: Image,
-}
-
-#[derive(Serialize, Deserialize)]
-pub struct Image {
-    pub full: String,
-    pub sprite: String,
-    pub group: String,
-    pub x: i64,
-    pub y: i64,
-    pub w: i64,
-    pub h: i64,
+    pub image: Image<String>,
 }
