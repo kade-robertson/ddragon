f4899209f841a84fe6086ff7fa40192166323908
Kade Robertson <kade@kaderobertson.dev>
2023-01-20 16:15:27 -0500

    fix(maps): use existing image struct

--- src/models/maps.rs
+++ src/models/maps.rs
@@ -1,10 +1,10 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
+use super::shared::Image;
+
 #[derive(Serialize, Deserialize)]
 pub struct Maps {
-    #[serde(rename = "type")]
-    pub maps_type: String,
     pub version: String,
     pub data: HashMap<String, Map>,
 }
@@ -15,16 +15,5 @@ pub struct Map {
     pub map_name: String,
     #[serde(rename = "MapId")]
     pub map_id: String,
-    pub image: Image,
-}
-
-#[derive(Serialize, Deserialize)]
-pub struct Image {
-    pub full: String,
-    pub sprite: String,
-    pub group: String,
-    pub x: i64,
-    pub y: i64,
-    pub w: i64,
-    pub h: i64,
+    pub image: Image<String>,
 }
