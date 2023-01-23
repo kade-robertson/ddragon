6e155b69041429196d83b7ac15d2ae93778246ed
Kade Robertson <kade@kaderobertson.dev>
2023-01-23 16:53:25 -0500

    fix: add missing derives

--- src/models/spell_buffs.rs
+++ src/models/spell_buffs.rs
@@ -1,12 +1,12 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct SpellBuffs {
     #[serde(rename = "spellBuffs")]
     pub spell_buffs: Vec<SpellBuff>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct SpellBuff {
     pub id: i64,
     pub name: String,
