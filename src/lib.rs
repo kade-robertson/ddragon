c1d1b1f6f395a5ca7512a2209ed91e4f7f293a35
Kade Robertson <kade@kaderobertson.dev>
2023-02-01 11:10:11 -0500

    refactor: create default-enabled sync feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
 #![doc = include_str!("../README.md")]
 
+/// Contains the structs that can be returned by the client API.
+pub mod models;
+
 #[cfg(feature = "local-cache")]
 /// Contains the local file caching middleware used for `ureq`.
 pub mod cache_middleware;
 
+#[cfg(feature = "sync")]
 /// Contains the client API.
 pub mod client;
 
-/// Contains the structs that can be returned by the client API.
-pub mod models;
-
+#[cfg(feature = "sync")]
 pub use client::DDragonClient;
