eb3f7f4ace6d55b344c4e9c8ecb7236bbb68013e
Kade <kade-robertson@users.noreply.github.com>
2023-02-02 07:27:44 -0500

    docs: remove unecessary docs (#27)

--- src/models/champions_full.rs
+++ src/models/champions_full.rs
@@ -1,16 +1,3 @@
-// Example code that deserializes and serializes the model.
-// extern crate serde;
-// #[macro_use]
-// extern crate serde_derive;
-// extern crate serde_json;
-//
-// use generated_module::ChampionsComplete;
-//
-// fn main() {
-//     let json = r#"{"answer": 42}"#;
-//     let model: ChampionsComplete = serde_json::from_str(&json).unwrap();
-// }
-
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
