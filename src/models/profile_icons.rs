60f3dd61c42686e1eb8ac63cde75c2c419892ee3
Kade Robertson <kade@kaderobertson.dev>
2023-02-04 09:53:04 -0500

    fix!: correctly name profile icon struct

--- src/models/profile_icons.rs
+++ src/models/profile_icons.rs
@@ -6,16 +6,16 @@ use super::shared::{has_image, Image};
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
 pub struct ProfileIcons {
     pub version: String,
-    pub data: HashMap<String, Datum>,
+    pub data: HashMap<String, ProfileIcon>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
-pub struct Datum {
+pub struct ProfileIcon {
     pub id: Id,
     pub image: Image,
 }
 
-has_image!(Datum);
+has_image!(ProfileIcon);
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
 #[serde(untagged)]
