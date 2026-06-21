687af208f0184fca474741a553b0cd4aa5b61916
Kade <kade-robertson@users.noreply.github.com>
2026-06-21 09:47:25 -0400

    fix(cdragon)!: support new augment rarity and data value structure (#108)
    * fix(cdragon)!: support new augment rarity and data value structure
    
    * chore: commit to force clippy

--- src/models/cdragon/augments.rs
+++ src/models/cdragon/augments.rs
@@ -10,17 +10,33 @@ pub struct AugmentsResponse {
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize_repr, Deserialize_repr)]
 #[repr(u8)]
+#[non_exhaustive]
 pub enum AugmentRarity {
     Silver = 0,
     Gold = 1,
     Prismatic = 2,
+    /// This is a terrible name, but this covers a mixture of:
+    /// - Ability augments
+    /// - Augments whose icons look like they'd be considered gold
+    /// - Maybe other stuff
+    ///
+    /// I don't know why it's implemented like this.
+    Misc = 4,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[non_exhaustive]
+#[serde(untagged)]
+pub enum DataValue {
+    Single(f64),
+    Scaling(Vec<f64>),
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Augment {
     pub api_name: String,
-    pub data_values: HashMap<String, f64>,
+    pub data_values: HashMap<String, DataValue>,
     pub desc: String,
     pub icon_large: String,
     pub icon_small: String,
